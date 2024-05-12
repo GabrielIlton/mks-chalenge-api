c47c1b30a4661f3ba230eeff6546620d882c7ba0
Gabriel Ilton <gabrielssantosjau@gmail.com>
2024-05-12 01:08:13 -0300

    feat: add dbhost

--- src/config/environments.ts
+++ src/config/environments.ts
@@ -5,6 +5,7 @@ ConfigModule.forRoot();
 export default {
   PORT: Number(process.env.PORT),
 
+  DB_HOST: process.env.DB_HOST,
   DB_NAME: process.env.DB_NAME,
   DB_PASSWORD: process.env.DB_PASSWORD,
   DB_PORT: Number(process.env.DB_PORT),
--- src/infra/database.module.ts
+++ src/infra/database.module.ts
@@ -7,6 +7,7 @@ import { Module } from '@nestjs/common';
     TypeOrmModule.forRootAsync({
       useFactory: async (configService: ConfigService) => ({
         type: 'postgres',
+        host: configService.get<string>('DB_HOST'),
         port: configService.get<number>('DB_PORT'),
         username: configService.get<string>('DB_USER'),
         password: configService.get<string>('DB_PASSWORD'),
