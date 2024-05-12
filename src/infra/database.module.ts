93732f46b70a4292b37e367539610c18e941e868
Gabriel Ilton <gabrielssantosjau@gmail.com>
2024-05-12 14:05:00 -0300

    refactor: improvements to connect database

--- src/infra/database.module.ts
+++ src/infra/database.module.ts
@@ -2,19 +2,22 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { ConfigService } from '@nestjs/config';
 import { Module } from '@nestjs/common';
 
+import { AccountEntity } from './entities/account.entity';
+import { MovieEntity } from './entities/movie.entity';
+
 @Module({
   imports: [
     TypeOrmModule.forRootAsync({
       useFactory: async (configService: ConfigService) => ({
         type: 'postgres',
+        ssl: true,
         host: configService.get<string>('DB_HOST'),
         port: configService.get<number>('DB_PORT'),
         username: configService.get<string>('DB_USER'),
         password: configService.get<string>('DB_PASSWORD'),
         database: configService.get<string>('DB_NAME'),
-        entities: [__dirname + '/entities/**'],
+        entities: [AccountEntity, MovieEntity],
         migrations: [__dirname + '/migrations/*.ts'],
-        ssl: true,
         synchronize: false,
       }),
       inject: [ConfigService],
